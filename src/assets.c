1a2cb53d5143fa816c1e0e3af5bcb1e49a857001
Trishvan637 <dabbirutrishvan@gmail.com>
2025-04-15 04:23:25 +0530

    more_refinement

--- src/assets.c
+++ src/assets.c
@@ -2,6 +2,9 @@
 #include "assets.h"
 #include "consts.h"
 
+
+Texture2D spriteSheet; 
+
 void LoadAssets() {
 	Image img = LoadImage("art/pieces.png");
 	ImageResizeNN(&img, SQUARE_SIZE * 6, SQUARE_SIZE * 2);
